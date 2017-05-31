36d4a6b1c4c55aa7ec0f1c98bdd8da1d36f872da
lipengju <liwangpingmysql@outlook.com>
2017-05-31 23:20:59 +0800

    新增了module,admin部分

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title','slug','pub_date',)
+
+admin.site.register(Post,PostAdmin)
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+#coding:utf-8
+
+from django.shortcuts import render,render_to_response,redirect
+from django.http import  HttpResponse,HttpResponseRedirect
 
 # Create your views here.
+
+def index(request):
+	return HttpResponse('Hello Django!')
