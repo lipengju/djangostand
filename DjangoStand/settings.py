a86cd312fca640b5f31c0545648f141982681692
lipengju <liwangpingmysql@outlook.com>
2017-06-04 18:01:06 +0800

    新增对样式的处理

--- DjangoStand/settings.py
+++ DjangoStand/settings.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#coding:utf-8
+
 """
 Django settings for DjangoStand project.
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainsite',
+    'markdown_deux',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+	os.path.join(BASE_DIR,'static')
+]
